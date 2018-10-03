5d95d61d05b01f63371dc92bcdc2a7ed8b37cc6c
Raymond <raima220@gmail.com>
2018-10-03 15:04:24 +0100

    LS

--- csv.go
+++ csv.go
@@ -1,19 +1,1 @@
-package response
-
-import (
-	"encoding/csv"
-	"net/http"
-)
-
-// CSV writes v as a CSV file to the ResponseWriter.
-func CSV(v [][]string, w http.ResponseWriter, status int) (err error) {
-	w.Header().Set("Content-Type", "text/csv")
-	w.WriteHeader(status)
-	csvw := csv.NewWriter(w)
-	err = csvw.WriteAll(v)
-	if err != nil {
-		return
-	}
-	csvw.Flush()
-	return
-}
+package responseimport (	"encoding/csv"	"net/http")// CSV writes v as a CSV file to the ResponseWriter.func CSV(v [][]string, w http.ResponseWriter, status int) (err error) {	w.Header().Set("Content-Type", "text/csv")	w.WriteHeader(status)	csvw := csv.NewWriter(w)	err = csvw.WriteAll(v)	if err != nil {		return	}	csvw.Flush()	return}
